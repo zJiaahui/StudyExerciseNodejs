2fccd97da37cd680d5fa099299727e9f823f8f32
Mrzhou-hr <211535176@qq.com>
2020-03-27 14:42:50 +0800

    解决评论时间bug

--- blog/router/home/comment.js
+++ blog/router/home/comment.js
@@ -4,10 +4,8 @@ const { Article } = require("../../model/article");
 const { Comment } = require("../../model/comment");
 module.exports = async (req, res) => {
   const { aid, uid, content } = req.body;
-  const time = Date.now();
-  console.log(time);
-
-  console.log(dateformat(time, "yyyy-mm-dd hh:mm:ss"));
-  //const comment = await Comment.create({ aid, uid, time, content });
+  const time = new Date();
+  console.log(dateformat(time, "yyyy-mm-dd hh:MM:ss"));
+  const comment = await Comment.create({ aid, uid, time, content });
   res.redirect("/home/article?id=" + aid);
 };
